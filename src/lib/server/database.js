59744c42e53b86aa4ac1591a654dcea527d252e8
c2002f <c2002fehr@csu.fullerton.edu>
2024-10-27 16:37:45 -0700

    database.js

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -4,6 +4,6 @@ import postgres from 'postgres'
 // see env variables in .env
 import {PGCONNECT} from '$env/static/private';
 
-const sql = postgres(PGCONNECT, {} )
+const sql = postgres(process.env.PGCONNECT, {} )
 
 export default sql;
