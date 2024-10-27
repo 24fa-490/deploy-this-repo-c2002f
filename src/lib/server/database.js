4e2551b31cae03b66bf25b47ca321fc875294728
c2002f <c2002fehr@csu.fullerton.edu>
2024-10-27 16:47:37 -0700

    pnpm-lock.yaml push

--- src/lib/server/database.js
+++ src/lib/server/database.js
@@ -1,9 +1,6 @@
+import postgres from 'postgres';
 
-import postgres from 'postgres'
 
-// see env variables in .env
-import {PGCONNECT} from '$env/static/private';
-
-const sql = postgres(process.env.PGCONNECT, {} )
+const sql = postgres(process.env.PGCONNECT, {});
 
 export default sql;
